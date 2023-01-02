9f3aa0e2e8590f1e11e0b2d90673e2290851a61b
Mandimby RAVELOARINJAKA <mandimby.raveloarinjaka@gmail.com>
2023-01-02 18:44:20 -0500

    [FIX] (watch): watch directories

--- src/space/mrave/one_time_utilities/babashka/fswatcher.clj
+++ src/space/mrave/one_time_utilities/babashka/fswatcher.clj
@@ -6,21 +6,61 @@
 
 (require '[pod.babashka.fswatcher :as fw])
 
-(defn watch-file
-  "Monitor a single file for file system events"
+(ns-unalias *ns* 'watch)
+
+(defmulti watch
+  "Monitor a resource for file system events"
+  (fn [& args] (let [source (first args)]
+                 (if (fs/directory? source) :dir :file)))
+  :default :file)
+
+(defn relevant?
+  [monitored-action event]
+  (or (= monitored-action :all) (= monitored-action (:type event))))
+
+(defmethod watch :file
+  watch-file
   [file-to-watch & {cb :callback
-                       action :type
-                       :or {action :all
-                            cb #(println "caught" %)}}]
+                    action :type
+                    :or {action :all
+                         cb #(println "caught" %)}}]
+  (println "watching file" file-to-watch)
   (let [parent (fs/parent (fs/file file-to-watch))]
     (fw/watch (str parent)
               (fn
                 [event]
-                (when (and (or (= action :all) (= action (:type event)))
+                (when (and (relevant? action event)
                            (= (fs/path file-to-watch) (fs/path (:path event))))
                   (cb event))))))
+
+(defmethod watch :dir
+  watch-dir
+  [dir-to-watch & {cb :callback
+                   action :type
+                   :or {action :all
+                        cb #(println "caught" %)}}]
+  (println "watching directory" dir-to-watch)
+  (fw/watch (str dir-to-watch)
+            (fn
+              [event]
+              (when (relevant? action event)
+                (cb event)))))
 
 (defn unwatch
   [watcher]
   (fw/unwatch watcher))
 
+(def watch-file (get-method watch :file))
+
+(comment
+  
+  (def watched-file (watch "D:\\code.py" :type :write))
+
+  (unwatch watched-file)
+
+  (def watched-dir (watch "D:\\" :type :write))
+
+  (unwatch watched-dir)
+
+
+  )
